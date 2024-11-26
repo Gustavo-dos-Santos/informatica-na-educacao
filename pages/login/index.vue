35493fe357f1d4bcff457869e5f02598c89182a4
Gustavo <gustavodossantosluiz@gmail.com>
2024-11-26 18:49:36 -0300

    :sparkles: importacao de dependencias

---
+++ pages/login/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <UForm :state="state">
+      <UFormgroup label="User">
+        <UInput v-model="state.user"></UInput>
+      </UFormgroup>
+      <UFormgroup label="Password">
+        <UInput v-model="state.password"></UInput>
+      </UFormgroup>
+      <UButton type="submit" label="Login"></UButton>
+    </UForm>
+
+    {{ state.user }}
+    {{ state.password}}
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+const state = ref({
+  user: "",
+  password: "",
+})
+
+const schema = Yup.
+
+</script>
+
+<style>
+
+</style>
